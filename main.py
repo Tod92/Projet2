125d30137f4d5e1539859cafb1863aa9d9fa40ca
thomas deruere <t.deruere@gmail.com>
2022-11-17 19:56:08 +0100

    recuperation de la page dans la variable response.text

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #livre choisi : http://books.toscrape.com/catalogue/naked_197/index.html
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://books.toscrape.com/catalogue/naked_197/index.html"
+
+response = requests.get(url)
+if response.ok:
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
