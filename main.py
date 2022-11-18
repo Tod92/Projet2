e70a47148991568d117db6a674209ea6c95df77c
thomas deruere <t.deruere@gmail.com>
2022-11-18 15:34:08 +0100

    première version du code qui renvoi une liste avec 4 des éléments demandés

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-#livre choisi : http://books.toscrape.com/catalogue/naked_197/index.html
-
 import requests
 from bs4 import BeautifulSoup
 
 url = "http://books.toscrape.com/catalogue/naked_197/index.html"
 
-response = requests.get(url)
-if response.ok:
-    print(response.text)
+def scrap_book(url):
+    """return liste contenant :
+    product_page_url
+    universal_ product_code (upc)
+    title
+    price_including_tax
+    price_excluding_tax
+    number_available
+    product_description
+    categoryreview_rating
+    image_url"""
+
+    product_page_url = url
+    response = requests.get(url)
+    if response.ok: #ne va pas plus loin si la connexion à la page web ne fonctionne pas
+        soup = BeautifulSoup(response.text) #recuperation du html de la page web dans la variable soup
+        tableau = soup.findAll("tr") #creation d'un liste "tableau" contenant tous les tableaux de la page
+        for element in tableau:
+            header = element.find("th")
+            if header.text == "UPC":
+                UPC = element.find("td").text
+            elif header.text == "Price (excl. tax)":
+                price_excl_tax = element.find("td").text
+            elif header.text == "Price (incl. tax)":
+                price_incl_tax = element.find("td").text
+        pass #definir quoi faire si pas de connexion à la page web
+    return [url,UPC,price_excl_tax,price_incl_tax]
 
 
-if __name__ == "__main__":
-    main()
+resultat = scrap_book(url)
+print(resultat)
