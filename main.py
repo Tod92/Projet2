701792da3650efb065a48dcecfb8b2f0c1c95816
Thomas Deruere <t.deruere@gmail.com>
2022-11-25 15:25:55 +0100

    finalisation partie scraping par categorie

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import requests, csv
 from bs4 import BeautifulSoup
 
-Base_url = "http://books.toscrape.com/"
-url = "http://books.toscrape.com/catalogue/naked_197/index.html"
+Base_url = "http://books.toscrape.com/catalogue/"
 Csv_header = ["product_page_url","UPC","title","price_incl_tax","price_excl_tax","number_available","product_description","categoryreview_rating","image_url"]
 
 def url_to_soup(url):
     """
     se connecte à l'url et renvoi un objet bs4.BeautifulSoup contenant tout le code html de la page
     """
-    soup = 0
     response = requests.get(url,"lxml") #"lxml" pour evite un message d'avertissement de bs4 à l'execution du code
     if response.ok: #ne va pas plus loin si la connexion à la page web ne fonctionne pas
         response.encoding = response.apparent_encoding #correction encodage pour les caractères s'afficheant mal
         soup = BeautifulSoup(response.text,features="html.parser") #recuperation du html de la page web dans la variable soup
+    else:
+        print("PB connexion à : "+ url)
+        soup = None
     return soup
+
+def url_remove_end(url):
+    """
+    fonction qui va modifer l'url reçue en entrée en retirant "index.html" ou "page-x.html"
+    renvoie la même url qu'en entrée si aucun des deux termes n'est trouvé
+    """
+    lenght = url.find("index.html")
+    if lenght != -1:
+        return url[:lenght]
+    lenght = url.find("page-")
+    if lenght != -1:
+        return url[:lenght]
+    return url
 
 
 
@@ -23,11 +37,18 @@ def scrap_category(url):
     url en entrée doit etre le lien de la page d'une categorie
     return une liste des urls de tous les livres de la catégorie
     """
-    articles = soup.findAll("article",{"class" : "product_pod"})
+    soup = url_to_soup(url)
+    articles = soup.findAll("article",{"class" : "product_pod"}) #recuperation de la liste des balises article correspondant à chaque livre de la page
     list_books_urls = []
     for article in articles:
         book_url = Base_url + article.a["href"]
+        book_url = book_url.replace("/..","") #nettoyage de l'url en retirant tous les "/.."
         list_books_urls.append(book_url)
+    nextpage = soup.find("li",{"class" : "next"}) #recherche la présence du bouton "next" pour savoir si une page suivante avec d'autres livres est presente
+    if nextpage: #dans le cas ou le bouton next existe
+        url_next = url_remove_end(url) + nextpage.a["href"]
+        list_next = scrap_category(url_next) #la fonction s'invoque elle-meme pour parcourir la page suivante
+        list_books_urls += list_next
 
     return list_books_urls
 
@@ -45,7 +66,6 @@ def scrap_book(url):
     categoryreview_rating
     image_url
     """
-
     product_page_url = url
     soup = url_to_soup(url)
     tableau = soup.findAll("tr") #creation d'un liste "tableau" contenant tous les tableaux de la page
@@ -76,8 +96,7 @@ def scrap_book(url):
             break
     encart_gauche = soup.find("div", {"class" : "col-sm-6"})
     image_url = Base_url + encart_gauche.img["src"]
-
-
+    image_url = image_url.replace("/..","")
     return [product_page_url,UPC,title,price_excl_tax,number_available,price_incl_tax,product_description,categoryreview_rating,image_url]
 
 def ajout_csv(liste):
@@ -93,10 +112,11 @@ def main():
     with open ("export.csv", "w",newline='') as csvfile:
             spamwriter = csv.writer(csvfile, delimiter=',')
             spamwriter.writerow(Csv_header)
-    resultat = scrap_book(url)
-    ajout_csv(resultat)
-    print(resultat)
-    print("fichier export.csv généré")
-
+    category_url = "http://books.toscrape.com/catalogue/category/books/mystery_3/"
+    list_books_urls = scrap_category(category_url)
+    for book_url in list_books_urls:
+        print("scraping book : " + book_url)
+        ajout_csv(scrap_book(book_url))
+    print("traitement terminé")
 if __name__ == '__main__':
     main()
